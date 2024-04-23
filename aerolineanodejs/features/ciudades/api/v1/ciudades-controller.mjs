eaaa8456f69f86402629d017428a928d8121324d
joseucarvajal <joseucarvajal@gmail.com>
2024-04-22 21:30:09 -0500

    Conexion NodeJS + Express + DB

--- aerolineanodejs/features/ciudades/api/v1/ciudades-controller.mjs
+++ aerolineanodejs/features/ciudades/api/v1/ciudades-controller.mjs
@@ -1,38 +1,37 @@
-import express from 'express';
+import express from "express";
+import CiudadesModel from "../../ciudades-model.mjs";
 
 const { Router } = express;
 
 export default class CiudadesController {
-
   #router = Router();
-
+  #ciudadesModel = null;
+  
   constructor() {
     this.registerRoutes();
   }
-
-  getRouter(){
+  
+  getRouter() {
     return this.#router;
   }
-
-
+  
   registerRoutes() {
     const routerV1 = Router();
-    routerV1.get(`/ciudades`, (req, res) => this.getAllCiudades(req, res));
-
+    routerV1.get(`/ciudades`, async (req, res) => await this.getAllCiudades(req, res));
+    
     this.#router.use(`/v1`, routerV1);
   }
-
+  
   async getAllCiudades(req, res) {
-    const ciudades = [
-      {
-        codigo: `BOG`,
-        nombre: `Bogota, Colombia`,
-      },
-      {
-        codigo: `ORY`,
-        nombre: `Paris, Francia controller`,
-      },
-    ];
-    res.json(ciudades);
+    try {
+      this.#ciudadesModel = new CiudadesModel();
+      this.#ciudadesModel.connect();
+      const ciudades = await this.#ciudadesModel.getAllCiudades();
+      res.json(ciudades);
+    } catch (error) {
+      console.error(`error: ${error}`);
+    } finally {
+      this.#ciudadesModel.closeConnection();
+    }
   }
 }
